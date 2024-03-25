9a95d04fd1dacbf2029cb1c6f44d75b082ca1c40
Seyeon Jo <seyeonjo.ud@gmail.com>
2024-03-25 15:32:31 -0400

    commit

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,7 +7,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
         Hi, I'm
-        <span className="font-semibold mx-2 text-white">Seyeon</span>
+        <span className="font-semibold mx-2 text-white">Seyeon Jo</span>
         👋
         <br />A UX designer from Toronto!
       </h1>
